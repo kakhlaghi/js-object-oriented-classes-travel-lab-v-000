1a2a7ab5c9f9627b24f59d4c546a093cac5db5ef
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 22:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver {
   }
 
   yearsExperienceFromBeginningOf(year){
-    let endDate = Date(year, 1, 1);
+    let endDate = new Date(year, 1, 1);
     let totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
    return parseInt(totalYears);
   }
